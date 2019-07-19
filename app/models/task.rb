afa418c2967f19622ffa61c79118a004b5ea8b6b
takumi aramaki <armktk0329m@gmail.com>
2019-07-19 11:26:40 +0900

    ADD: Taskモデルにpriorityのenumを追加, ja.ymlに優先順位の日本語を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,6 @@ class Task < ApplicationRecord
 
     scope :search_status, -> (params){ where(status: params) }
     scope :search_title, -> (params){ where('title LIKE ?', "%#{params}%") }
+
+    enum priority:{"高":0, "中":1, "低":2}
 end
