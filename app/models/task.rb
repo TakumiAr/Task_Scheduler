08c7314d298646f9de2607da11fb6082de17862a
takumi aramaki <armktk0329m@gmail.com>
2019-07-24 19:03:56 +0900

    Add: ラベル登録機能を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -7,4 +7,7 @@ class Task < ApplicationRecord
 
     enum priority:{"高":0, "中":1, "低":2}
     belongs_to :user
+    has_many :labelings, dependent: :destroy
+    has_many :labels, through: :labelings
+    accepts_nested_attributes_for :labelings
 end
